ad5d6564f6acf92e291d1ea49d91c4391baf29e0
MarcoMeyer1 <marcomeyerbusiness@gmail.com>
2024-10-17 16:35:15 +0200

    Made admin dashboard, only accessable by admins

---
+++ Services/UserState.cs
@@ -0,0 +1,30 @@
+﻿namespace NumNumFoods.Services
+{
+    public class UserState
+    {
+        public event Action OnChange;
+
+        private string _role;
+
+        public string Role
+        {
+            get => _role;
+            private set
+            {
+                if (_role != value)
+                {
+                    _role = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+
+        public void SetRole(string role)
+        {
+            Role = role;
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+
+}

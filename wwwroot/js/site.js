c85a2472a0a557f12ae92b515702a5717393f2ab
LukeJohnson03 <lukejohnsonlgj905@gmail.com>
2024-10-23 00:08:55 +0200

    I cry :(

---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿window.initializeBootstrap = () => {
+    var toggler = document.querySelector('.navbar-toggler');
+    var collapseDiv = document.querySelector('#navbarNav');
+
+    toggler.addEventListener('click', function () {
+        collapseDiv.classList.toggle('show');
+    });
+};

2d8dc5509037588da7a8d1da8e9bba7e815ffdbf
MarcoMeyer1 <marcomeyerbusiness@gmail.com>
2024-10-22 17:09:24 +0200

    Product images work

--- Models/ProductCreateRequest.cs
+++ Models/ProductCreateRequest.cs
@@ -1,11 +1,19 @@
-﻿namespace NumNumFoods.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NumNumFoods.Models
 {
     public class ProductCreateRequest
     {
+        [Required]
         public string Name { get; set; }
+
         public string Description { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+
         public int StockQuantity { get; set; }
+
         public int? CategoryId { get; set; } // Optional: Category ID for the product
 
         public string? StockStatus { get; set; } // Stock status of the product
